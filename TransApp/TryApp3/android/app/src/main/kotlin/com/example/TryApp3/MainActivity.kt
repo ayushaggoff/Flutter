5b3f474b17e311a187cd6223e5d9c1e541aa5eb5
Ayush Aggarwal <ayusha@successive.com>
2020-07-24 21:20:05 +0530

    Transaction Management App

---
+++ TransApp/TryApp3/android/app/src/main/kotlin/com/example/TryApp3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.TryApp3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ TransApp/android/app/src/main/kotlin/com/example/TransApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.TransApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
